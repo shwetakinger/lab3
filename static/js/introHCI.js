8b5888718fd178f84489be7e2c1220eb7aa8ab51
Shweta Kinger <skinger@ucsd.edu>
2021-01-21 10:38:46 -0800

    added js function and css changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,29 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Button is clicked");
+		$(".jumbotron p").addClass("active");
+
 	});
+	$("a.thumbnail").click(projectClick);
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+	console.log("in projectClick");
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").hide();
+    }
+
 }
